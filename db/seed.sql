c56e8e1d986c0b944b4aedfa2f44deb3e1792f36
jakevs <jakevs3@gmail.com>
2020-07-03 19:43:01 -0500

    added index.js in models and added to seeds.sql

---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_type, eaten) VALUES ("Bacon Burger", FALSE);
+INSERT INTO burgers (burger_type, eaten) VALUES ("American Burger", TRUE);
+INSERT INTO burgers (burger_type, eaten) VALUES ("Black Bean Burger", FALSE);
