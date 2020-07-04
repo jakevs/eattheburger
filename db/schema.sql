252e27eaf3ba9b181acbc79f8642747f33b42c3f
jakevs <jakevs3@gmail.com>
2020-07-03 20:04:17 -0500

    added to gitignore, fixed schema error, and modified packagejson

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,7 @@ DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE bugers (
+CREATE TABLE burgers (
     id INTEGER AUTO_INCREMENT NOT NULL,
     burger_type VARCHAR(225),
     eaten BOOLEAN DEFAULT FALSE,
