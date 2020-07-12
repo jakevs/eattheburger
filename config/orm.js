21c0c50d605f50969253ab64eea673b942ddd12d
jakevs <jakevs3@gmail.com>
2020-07-12 03:49:36 -0500

    adding more to orm

--- config/orm.js
+++ config/orm.js
@@ -1,37 +1,34 @@
 // Import MySQL connection.
 var connection = require("../config/connection.js");
 
-var orm = {
+async function selectAll(){
+  const query = `SELECT * FROM burgers`;
+  const result = await connection.query(query);
+  console.table(result)
+  return result
+}
 
-  selectAll: function (cb) {
-      var queryString = "SELECT * FROM burgers";
-      connection.query(queryString, function (err, result) {
-          if (err) {
-              throw err;
-          }
-          cb(result);
-      });
-  },
+async function insertOne(add_burger){
+  const query = `INSERT INTO burgers (burger_type) VALUES ("${add_burger}");`;
+  const result = await connection.query(query)
+  console.table(result)
+  return result;
+}
 
-  insertOne: function (db, cb) {
-      var queryString = "INSERT INTO burgers (burger_type) VALUES (?)";
-      connection.query(queryString, [db], function (err, result) {
-          if (err) {
-              throw err;
-          }
-          cb(result);
-      });
-  },
+async function updateOne(id){
+  const query = `
+      UPDATE burgers
+      SET devoured = true
+      WHERE id = ${id}
+  `;
+  const result = await connection.query(query)
+  console.table(result)
+  return result;
+}
 
-  updateOne: function (id, cb) {
-      var queryString = "UPDATE burgers SET eaten = true WHERE id = ?";
-
-      connection.query(queryString, [id], function (err, result) {
-          if (err) {
-              throw err;
-          }
-          cb(result);
-      });
-  }
-};
+const orm = {
+  selectAll: selectAll,
+  insertOne: insertOne,
+  updateOne: updateOne,
+}
 module.exports = orm;
