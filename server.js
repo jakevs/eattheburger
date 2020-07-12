31824191e709817b3a546cc2d61050dee1d7e364
jakevs <jakevs3@gmail.com>
2020-07-11 23:51:40 -0500

    fixing server.js

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require("express");
+const methodOverride = require('method-override');
 const parser = require("body-parser");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-const db = require("./models");
-
-
 app.use(parser.urlencoded({
     extended: true
 }));
 app.use(parser.json());
+
+app.use(methodOverride("_method"));
 
 app.use(express.static("public"));
 
@@ -24,8 +24,10 @@ app.set("view engine", "handlebars");
 const routes = require("./controllers/controller.js");
 app.use(routes);
 
-db.sequelize.sync().then(function() {
-    app.listen(PORT, function() {
-        console.log("Listening on PORT" + PORT);
-    })
-})
+app.listen(PORT);
+
+// db.sequelize.sync().then(function() {
+//     app.listen(PORT, function() {
+//         console.log("Listening on PORT" + PORT);
+//     })
+// })
