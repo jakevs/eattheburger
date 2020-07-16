4559948ea51175cb513304372f3b5168f97de0de
James Van Slyke <60765773+jakevs@users.noreply.github.com>
2020-07-15 21:43:58 -0500

    trying to get jawsdb connection to heroku to work

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,23 @@
 var mysql = require("mysql");
 const util = require("util");
 
-var connection = mysql.createConnection({
-  host: 'localhost',
-      user: 'root',
-      password: 'password',
-      database: 'burgers_db'
-})
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//   connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'password',
-//     database: 'burgers_db'
-//   });
-// }
+var connection;
+//= mysql.createConnection({
+//   host: 'localhost',
+//       user: 'root',
+//       password: 'password',
+//       database: 'burgers_db'
+// })
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'burgers_db'
+  });
+}
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
