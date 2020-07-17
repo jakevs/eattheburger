ac0627d7847b702f21b8c63567dbe64df59b949b
James Van Slyke <60765773+jakevs@users.noreply.github.com>
2020-07-16 21:54:06 -0500

    got heroku db working

--- db/seed.sql
+++ db/seed.sql
@@ -2,4 +2,3 @@ INSERT INTO burgers (burger_type, eaten) VALUES ("Bacon Burger", FALSE);
 INSERT INTO burgers (burger_type, eaten) VALUES ("American Burger", TRUE);
 INSERT INTO burgers (burger_type, eaten) VALUES ("Black Bean Burger", FALSE);
 
-SELECT * FROM burgers;
